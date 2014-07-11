4be4502c32cbb5c9302c614a258ddf3140a6a788
John Andersen <johnx.s.andersen@intel.com>
2014-07-11 01:26:19 -0700

    Added client server file transfer

--- client.py
+++ client.py
@@ -1,11 +1,93 @@
 #! /usr/bin/python
 
-import socket
-import sys
+import socket, sys, StringIO, json, os
 from server import transfer
 
 def main():
-	transfer().send( "/home/johnny/Documents/test/client/test1.txt" )
+    handleInput(sys.argv[1:])
+    while takeInput() is True:
+        pass
+
+def takeInput():
+    global user;
+    userinput = input('#: ');
+    userinput = userinput.split();
+    if "exit" in userinput:
+        if userinput.index("exit") is 0:
+            print("Bye");
+            return False;
+    elif "logout" in userinput:
+        if userinput.index("logout") is 0:
+            print("Bye");
+            return False;
+    handleInput(userinput);
+    return True;
+
+def readFile( fileName ):
+    file = open( fileName, "rb" )
+    for line in file:
+        print( line )
+    file.close()
+
+def handleInput(userinput):
+    #global user;
+    #global db;
+    #global table;
+    global transfer;
+    if "file" in userinput and userinput.index("file") is 0:
+        transfer.sendText( userinput[1] )
+        if transfer.reciveFile( "/home/johnny/Documents/test/client/", readFile ):
+            return True
+    elif "insert" in userinput and userinput.index("insert") is 0:
+        pre = userinput[2:];
+        obj = {};
+        for i in pre:
+            obj[i.split(":")[0]] = i.split(":")[1];
+        print(userinput[1])
+        printObject(obj)
+        user[userinput[1]] = obj;
+    elif "use" in userinput and userinput.index("use") is 0:
+        db = userinput[1];
+        if os.path.exists("dbs/"+db):
+            print("Database "+userinput[1]+" contains:")
+            printArray( os.listdir("dbs/"+db) )
+        else:
+            print("Database doesn't exist")
+    elif "create" in userinput and userinput.index("create") is 0:
+        create(userinput[1:])
+    elif "show" in userinput and userinput.index("show") is 0:
+        show(userinput[1:])
+    elif "select" in userinput and userinput.index("select") is 0:
+        select(userinput[1:])
+    elif "save" in userinput and userinput.index("save") is 0:
+        if userinput[1] == "all":
+            f = open(userinput[2] , 'w');
+            f.close();
+            for i in user:
+                writeObject(userinput[2], i, user[i]);
+        elif userinput[1] in user:
+            writeObject(userinput[2], userinput[1], user[userinput[1]]);
+        else:
+            print("Sorry no JSONs in memory");
+    elif "delete" in userinput and userinput.index("delete") is 0:
+        if userinput[1] == "all":
+            user = {};
+        elif userinput[1] in user:
+            del user[userinput[1]];
+        else:
+            print("Sorry that JSON is not in memory");
+    elif "load" in userinput and userinput.index("load") is 0:
+        print( "Loading JSONs from " + userinput[1] );
+        loadJsonsFromFile(userinput[1], user);
+        printObject(user);
+    elif "modify" in userinput and userinput.index("modify") is 0:
+        print( "Modifing " + userinput[1] + ", currently: " );
+        printObject(user[userinput[1]])
+        change = input(userinput[1]+": ").split();
+        user[userinput[1]][change[0]] = change[1];
+    else:
+        return 0;
 
 if __name__ == "__main__":
+    transfer = transfer()
     main()
--- server.py
+++ server.py
@@ -1,28 +1,36 @@
 #! /usr/bin/python
 
-import socket
-import sys
+import socket, sys, StringIO, json, os
 
 class transfer(object):
     serverAddress = "localhost"
     serverPort = 9999
-    def send( self, fileLocation ):
+    myAddress = "localhost"
+    myPort = 9999
+    connections = {'self': serverAddress, }
+    def sendFile( self, fileLocation ):
         s = socket.socket()
         s.connect( ( self.serverAddress, self.serverPort ) )
         f=open (fileLocation, "rb") 
         l = f.read(1024)
         while (l):
-           s.send(l)
-           l = f.read(1024)
+            s.send(l)
+            l = f.read(1024)
+        s.close()
+
+    def sendText( self, text ):
+        s = socket.socket()
+        s.connect( ( self.serverAddress, self.serverPort ) )
+        for char in text:
+            s.send(char)
         s.close()
     
-    def recive( self, saveLocation="./", onComplete=None ):
+    def reciveFiles( self, saveLocation="./", onComplete=None, onCompleteParam=None ):
         s = socket.socket()
-        s.bind( ( self.serverAddress, self.serverPort ) )
+        s.bind( ( self.myAddress, self.myPort ) )
         s.listen(10)
         while True:
             sc, address = s.accept()
-            print address
             i = 0
             fileName = saveLocation+'file_'+ str(i)+".txt"
             f = open( fileName,'wb')
@@ -33,20 +41,71 @@ class transfer(object):
                     f.write(l)
                     l = sc.recv(1024)
                 break
-            print( "Done writing file "+fileName )
             f.close()
             if onComplete:
-                onComplete( fileName )
+                if onCompleteParam:
+                    onComplete( fileName, onCompleteParam )
+                else:
+                    onComplete( fileName )
             sc.close()
         s.close()
+    
+    def reciveFile( self, saveLocation="./", onComplete=None, onCompleteParam=None ):
+        s = socket.socket()
+        s.bind( ( self.myAddress, self.myPort ) )
+        s.listen(10)
+        sc, address = s.accept()
+        i = 0
+        fileName = saveLocation+'file_'+ str(i)+".txt"
+        f = open( fileName,'wb')
+        i += 1
+        while (True):       
+            l = sc.recv(1024)
+            while (l):
+                f.write(l)
+                l = sc.recv(1024)
+            break
+        f.close()
+        if onComplete:
+            if onCompleteParam:
+                onComplete( fileName, onCompleteParam )
+            else:
+                onComplete( fileName )
+        sc.close()
+        s.close()
+    
+    def reciveText( self, saveLocation="./", onComplete=None, onCompleteParam=None ):
+        s = socket.socket()
+        s.bind( ( self.serverAddress, self.serverPort ) )
+        s.listen(10)
+        sc, address = s.accept()
+        text = StringIO.StringIO()
+        while (True):
+            l = sc.recv(1024)
+            while (l):
+                text.write(l)
+                l = sc.recv(1024)
+            request = text.getvalue()
+            text.close()
+            sc.close()
+            s.close()
+            return address, request
 
-def addToFile( path ):
+
+def addToFile( path, text="Addition to file" ):
     f = open( path,'a')
-    f.write("Hey I got the file")
+    f.write(text)
     f.close()
+    print( "Done writing file "+fileName )
 
 def main():
-    transfer().recive( "/home/johnny/Documents/test/server/", addToFile )
+    global transfer
+    while ( True ):
+        (address, fileName) = transfer.reciveText()
+        transfer.serverAddress = address
+        transfer.sendFile( "/home/johnny/Documents/test/server/"+fileName )
+
 
 if __name__ == "__main__":
+    transfer = transfer()
     main()
